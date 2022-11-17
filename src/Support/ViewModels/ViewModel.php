2dc5de5073742314338cc198b8274331fb1c7dc4
Vadim0802 <v.bodyansky@yandex.ru>
2022-11-17 23:29:23 +1000

    Some refactors for more readability

--- src/Support/ViewModels/ViewModel.php
+++ src/Support/ViewModels/ViewModel.php
@@ -6,7 +6,6 @@ namespace Support\ViewModels;
 
 use Closure;
 use Illuminate\Contracts\Support\Arrayable;
-use Illuminate\Support\Str;
 use ReflectionClass;
 use ReflectionMethod;
 
@@ -21,18 +20,25 @@ abstract class ViewModel implements Arrayable
         $class = new ReflectionClass($this);
 
         $methods = collect($class->getMethods(ReflectionMethod::IS_PUBLIC))
-            ->reject(function (ReflectionMethod $method) {
-                return str($method->getName())->startsWith('__')
-                    || in_array($method->getName(), static::IGNORED_METHODS);
-            })
+            ->reject(fn (ReflectionMethod $method) => $this->shouldIgnore($method))
             ->mapWithKeys(function (ReflectionMethod $method) {
-                return [str($method->getName())->snake()->value() => $this->createVariableFromMethod($method)];
+                return [$this->formattingKey($method) => $this->createVariableFromMethod($method)];
             });
 
         return $methods->toArray();
     }
 
-    protected function createVariableFromMethod(ReflectionMethod $method): mixed
+    private function formattingKey(ReflectionMethod $method): string
+    {
+        return str($method->getName())->snake()->value();
+    }
+
+    private function shouldIgnore(ReflectionMethod $method): bool
+    {
+        return str($method->getName())->startsWith('__') || in_array($method->getName(), static::IGNORED_METHODS);
+    }
+
+    private function createVariableFromMethod(ReflectionMethod $method): mixed
     {
         if ($method->getNumberOfParameters() == 0) {
             return $this->{ $method->getName() }();
